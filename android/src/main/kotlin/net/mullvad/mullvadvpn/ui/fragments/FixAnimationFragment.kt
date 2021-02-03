0d06e7474716e04b20d282a2d23c375e4e244c90
Aleksandr Granin <aleksandr@mullvad.net>
2021-02-03 12:55:29 +0100

    Fix fragment transition animation Z-order

---
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/fragments/FixAnimationFragment.kt
@@ -0,0 +1,20 @@
+package net.mullvad.mullvadvpn.ui.fragments
+
+import android.view.animation.Animation
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.Fragment
+import net.mullvad.mullvadvpn.R
+
+abstract class FixAnimationFragment : Fragment() {
+    override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {
+        val zAdjustment = when (nextAnim) {
+            R.anim.fragment_enter_from_right,
+            R.anim.fragment_exit_to_right,
+            R.anim.fragment_enter_from_bottom,
+            R.anim.fragment_exit_to_bottom -> 1f
+            else -> 0f
+        }
+        ViewCompat.setTranslationZ(view!!, zAdjustment)
+        return super.onCreateAnimation(transit, enter, nextAnim)
+    }
+}
